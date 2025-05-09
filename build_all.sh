f9b693606b411a9a6bc23acdd1a96e236c1b0f09
0x0015 <64482595+0x0015@users.noreply.github.com>
2025-05-08 20:00:52 -0400

    initial commit

---
+++ build_all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+cd dwl
+echo $(pwd)
+cd wlroots
+meson setup build
+ninja -C build
+cd ..
+make -j
+cd ..
+
+cd secondScreenUtil
+mkdir build
+cd build
+cmake ..
+make -j
+cd ..
+cd ..
+
+mkdir build
+cp dwl/dwl build
+cp dwl/wlroots/build/libwlroots-0.19.so build
+cp secondScreenUtil/build/secondScreenUtil build
+
---
+++ clean_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+cd dwl
+cd wlroots
+rm -rf build
+cd ..
+make clean
+cd ..
+
+cd secondScreenUtil
+rm -rf build
+cd ..
+
+rm -rf build
+
