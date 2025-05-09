bd918dadf579f416e49abc4b418207949da512a3
0x0015 <64482595+0x0015@users.noreply.github.com>
2025-05-09 00:01:30 -0400

    bump

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sudo steomos-readonly disable
 sudo pacman -Sy
-sudo pacman -S meson ninja cmake
+sudo pacman -S cmake
 sudo pacman -U prereq/wayland-protocols-1.44-1-any.pkg.tar.zst
 sudo steamos-readonly enable
 
