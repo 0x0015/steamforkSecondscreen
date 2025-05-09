0718d6cf1a81b0aa5e7cd7d131f67c60ac616163
0x0015 <64482595+0x0015@users.noreply.github.com>
2025-05-08 20:25:53 -0400

    improved launch command

--- files/dualscreenLaunch.sh
+++ files/dualscreenLaunch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd /home/deck/steamforkSecondscreen/build;./dwl
+export XDG_SESSION_TYPE=wayland
+export XDG_RUNTIME_DIR=/run/user/$(id -u)  # required by Wayland apps
+export XDG_SESSION_DESKTOP=dwl
+
+cd /home/deck/steamforkSecondscreen/build
+./dwl &
 sleep 2
-/usr/lib/steamos/gamescope-session
-/home/deck/steamforkSecondscreen/build/secondScreenUtil
+/usr/lib/steamos/gamescope-session &
+/home/deck/steamforkSecondscreen/build/secondScreenUtil &
+
